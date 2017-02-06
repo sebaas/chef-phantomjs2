8be42f0d736343fe03991922677dd53a6002f31d
Sebastian <seba.as@gmail.com>
2017-02-06 16:14:03 -0300

    Reuse local hash as var

--- resources/default.rb
+++ resources/default.rb
@@ -6,7 +6,7 @@ attribute :version, kind_of: String, default: lazy { node['phantomjs2']['version
 attribute :checksum, kind_of: [String, NilClass], default: lazy { node['phantomjs2']['checksum'] }
 attribute :packages, kind_of: Array, default: lazy { node['phantomjs2']['packages'] }
 attribute :base_url, kind_of: String, default: lazy { node['phantomjs2']['base_url'] }
-attribute :basename, kind_of: String, default: lazy { "phantomjs-2.1.1-linux-#{node['kernel']['machine']}" }
+attribute :basename, kind_of: String, default: lazy { "phantomjs-#{node['phantomjs2']['version']}-linux-#{node['kernel']['machine']}" }
 attribute :link, kind_of: [TrueClass, FalseClass], default: true
 attribute :user, kind_of: String, default: 'root'
 attribute :group, kind_of: String, default: 'root'
